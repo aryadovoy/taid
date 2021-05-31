65576e1c3ab5fae7ca61b29d8ecb99ca7ae87c40
aryadovoy <ryav.omsk@gmail.com>
2021-05-31 08:20:24 +0400

    added secret.template and deleted all comments from taid.py, changed README.md and .gitignore

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # taid
 
-**TelegramAID** is tool for more convenient chatting in Telegram. This's fork of [opentfd](https://github.com/SlavikMIPT/opentfd).
+**TelegramAID** is tool for more convenient chatting in Telegram. This's fork of [opentfd](https://github.com/SlavikMIPT/opentfd) (now have some bugs and doesn't work properly).
 
 ### Functions:
 
-- Merges series of Telegram messages if there is a pause of less than 30 seconds between them.
+- Merges series of Telegram messages if there is a pause of less than 30 seconds (you can change this) between them.
 - Bash assistant — type `!bash command` in any chat to execute command on your host.
+- Getting available music-links from [@OdesliBot](https://t.me/odesli_bot), when you send links from Spotify or Yandex.Music, and replacing you message to this links.
 
 ### To-do:
 
 - [ ] Fix the problems:
 	- [x] Ignoring incoming message in the chat.
-	- [ ] False deleting of messages instead of merging.
+	- [ ] False deleting of messages after forwarding.
 - [x] Forwading messages with music-links to [@OdesliBot](https://t.me/odesli_bot) and replacing your message to bot's message.
 	- [ ] Fix the double sending to [@OdesliBot](https://t.me/odesli_bot) in OdesliBot's chat.
 - [ ] Docker image with tool.
@@ -25,4 +26,3 @@
 ### Dependencies
 
 * Latest version of Telethon: http://telethon.readthedocs.io/en/stable/
-* Translator: https://github.com/mouuff/mtranslate.git	
--- taid.py
+++ taid.py
@@ -3,11 +3,8 @@ from datetime import timedelta
 from time import time, sleep
 from contextlib import suppress
 
-# import mtranslate
 from telethon import TelegramClient, events, sync, errors, custom
-from telethon.tl.types import UpdateDraftMessage
 from proxy import mediatube_proxy
-# from supported_langs import supported_langs
 import secret
 import getopt
 import re
@@ -24,17 +21,12 @@ except getopt.GetoptError:
     sys.exit(2)
 
 client = TelegramClient('taid_session', secret.api_id, secret.api_hash, proxy=default_proxy).start()
-# last_msg = None
-# break_time = None
-# last_msg_time = time()
 chat_id = 1
 state = None
 state_time = 0
 msg_flag = True
 time_flag = False
 MERGE_TIMEOUT = 30
-merge_semaphore = asyncio.Semaphore(value=1)
-draft_semaphore = asyncio.Semaphore(value=1)
 
 
 @client.on(events.NewMessage(outgoing=True, pattern=r'^.*(open\.spotify\.com|music\.yandex\.ru).*'))
@@ -89,8 +81,6 @@ async def merger(event: custom.Message):
         state = event
         msg_flag = True
         state_time = int(time())
-    # print('Time flag is', time_flag)
-    # print('Message flag is', msg_flag)
 
 
 async def run_command_shell(cmd, e):
@@ -146,109 +136,4 @@ async def bash(e: events.NewMessage.Event):
         print('timeout!')
 
 
-"""
-@client.on(events.Raw(types=UpdateDraftMessage))
-async def translator(event: events.NewMessage.Event):
-    global draft_semaphore
-    await draft_semaphore.acquire()
-    try:
-        draft_list = await client.get_drafts()
-        for draft in draft_list:
-            if draft.is_empty:
-                continue
-            text = draft.text
-            for lang_code in supported_langs.values():
-                if text.endswith('/{0}'.format(lang_code)):
-                    translated = mtranslate.translate(text[:-(len(lang_code) + 1)], lang_code, 'auto')
-                    for i in range(3):
-                        try:
-                            await draft.set_message(text=translated)
-                            await asyncio.sleep(7)
-                            return
-                        except Exception as e:
-                            print(e)
-    except Exception as e:
-        print(e)
-    finally:
-        draft_semaphore.release()
-
-
-@client.on(events.NewMessage(pattern=r'^!type->.*', outgoing=True))
-async def typing_imitate(message: events.NewMessage.Event):
-    text, text_out = str(message.raw_text).split('->')[-1], str()
-    word = list(text)
-    with suppress(Exception):
-        for letter in word:
-            text_out += letter
-            try:
-                if word.index(letter) % 2 == 1:
-                    await message.edit(f'`{text_out}`|')
-                else:
-                    await message.edit(f'`{text_out}`')
-                await asyncio.sleep(0.2)
-            except errors.MessageNotModifiedError:
-                continue
-
-
-@client.on(events.NewMessage(incoming=True))
-async def break_updater(event: events.NewMessage.Event):
-    global break_time
-    global last_msg
-    with suppress(Exception):
-        if event.chat and event.chat.bot:
-            return
-    if last_msg:
-        try:
-            if (event.message.to_id.user_id == last_msg.from_id and
-                    last_msg.to_id.user_id == event.message.sender_id):
-                break_time = time()
-        except Exception:
-            if event.to_id == last_msg.to_id:
-                break_time = time()
-
-
-@client.on(events.NewMessage(outgoing=True))
-async def merger(event: custom.Message):
-    global last_msg
-    global break_time
-    global last_msg_time
-    global merge_semaphore
-
-    event_time = time()
-    with suppress(Exception):
-        if event.text:
-            if event.text.startswith('!bash'):
-                return
-        with suppress(Exception):
-            if event.chat:
-                if event.chat.bot:
-                    return
-        if (event.media or event.fwd_from or event.via_bot_id or
-                event.reply_to_msg_id or event.reply_markup):
-            last_msg = None
-        elif last_msg is None:
-            last_msg = event
-        elif last_msg.to_id == event.to_id:
-            if break_time:
-                if break_time < event_time:
-                    last_msg = event
-                    last_msg_time = event_time
-                    break_time = 0
-            elif event_time - last_msg_time < MERGE_TIMEOUT:
-                try:
-                    await merge_semaphore.acquire()
-                    last_msg = await last_msg.edit('{0}\n{1}'.format(last_msg.text, event.text))
-                    last_msg_time = event_time
-                    await event.delete()
-                finally:
-                    merge_semaphore.release()
-            else:
-                last_msg = event
-                last_msg_time = event_time
-        else:
-            last_msg = event
-            last_msg_time = event_time
-"""
-
-# print('\n'.join([f'{k:<25}/{v}' for k, v in supported_langs.items()]))
 client.run_until_disconnected()
